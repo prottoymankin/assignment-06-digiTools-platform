e10034085295a6f1d2a0a88428bbe8e871b37ac5
Prottoy Mankin <prottoy032020@gmail.com>
2026-03-30 22:06:03 +0600

    update stats style

--- src/components/Stats/Stat.jsx
+++ src/components/Stats/Stat.jsx
@@ -1,7 +1,7 @@
 const Stat = ({ count, text, isMiddleState }) => {
   return (
     <div 
-      className={ `space-y-3 text-center py-4 border-[#9514FA] border-t-3 md:border-t-0 border-b-3 md:border-b-0 ${isMiddleState && 'md:border-l-3 md:border-r-3'}` }
+      className={ `space-y-3 text-center py-4 border-[#9514FA]  ${isMiddleState && 'md:border-l-3 md:border-r-3 border-t-3 md:border-t-0 border-b-3 md:border-b-0'}` }
     >
       <h2 
         className="font-extrabold text-4xl sm:text-5xl lg:text-6xl"
